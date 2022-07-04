dd136a12cb7a8cff7c52dcadd9762feb9921a691
luizfcp <luizfcpassos@hotmail.com>
2022-07-04 13:15:39 -0300

    painel educacional

--- scripts/6. Painel - Censo Educacional 2006 e 2017.R
+++ scripts/6. Painel - Censo Educacional 2006 e 2017.R
@@ -181,4 +181,4 @@ gc()
 
 # -------------------------------------------------------------------------
 
-# painel_educacional %>% writexl::write_xlsx(path = "output/painel_educacional.xlsx")
+saveRDS(painel_educacional, "output/painel_educacional.rds")
